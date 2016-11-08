ecd1eeaca0a2b3cfd54e12ebb3fc7636b98def3d
Darren Shewry <dshewry@dfjs.co.uk>
2016-11-08 17:50:04 +0000

    add http request

--- src/About.js
+++ src/About.js
@@ -1,9 +1,26 @@
 import React, { PropTypes, Component } from 'react';
+import 'whatwg-fetch';
 
 export default class About extends Component {
   // static propTypes = {}
   // static defaultProps = {}
   // state = {}
+
+  constructor () {
+    super();
+
+    this.state = {
+      url: ''
+    }
+  }
+
+  componentWillMount() {
+    fetch('http://httpbin.org/delay/5')
+      .then((response) => {
+        console.log('response', response.url);
+        this.setState({ url: response.url });
+      })
+  }
 
   render() {
     const { className, ...props } = this.props;
@@ -12,8 +29,8 @@ export default class About extends Component {
         <h1>
           About
         </h1>
+        <p>{this.state.url}</p>
       </div>
     );
   }
 }
-
